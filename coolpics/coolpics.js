db9b4ae52e05f1157addf8b945f963ab2a0e75a9
IsaacMadson <isaac.madson@hotmail.com>
2025-05-13 10:19:41 -0600

    new

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,2 +1,28 @@
-document.querySelector('h2').addEventListener('click', function () {
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  const button = document.querySelector(".button");
+  const menu = document.querySelector(".hide");
+
+  function toggleMenu() {
+    menu.classList.toggle("hide");
+  }
+
+
+
+  button.addEventListener("click", toggleMenu);
+
+  function handleResize() {
+  const menu = document.querySelector("nav");
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
 });
